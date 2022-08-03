75cbd0087a01986ecda278f1ac42689b7909d61e
hk8817 <97877660+hk8817@users.noreply.github.com>
2022-08-03 21:11:33 +0530

    Update plugin.rb

--- plugin.rb
+++ plugin.rb
@@ -1,11 +1,11 @@
-# name: feature-phone-first
+# name: HK Custom Code
 # version: 0.1
 
 after_initialize do
   ApplicationController.class_eval do
     def set_layout
       if basic?
-        File.expand_path('../app/views/layouts/basic.html.erb', __FILE__)
+        File.expand_path('../app/views/topics/show.html.erb', __FILE__)
       else
         'application'
       end
